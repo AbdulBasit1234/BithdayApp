ae73418107641437fcf007fd34f36f2eec8c4af5
abdul <iteb4.basit@gmail.com>
2017-10-01 07:40:03 -0700

    Initial commit

---
+++ app/src/main/java/com/example/abdul/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.abdul.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import static android.content.Intent.ACTION_SENDTO;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
